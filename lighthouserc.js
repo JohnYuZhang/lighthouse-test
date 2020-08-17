a4077b030836b74e3941197d2e6dd50e962d66a1
John Zhang <john.zhang.gyz@gmail.com>
2020-08-16 21:30:39 -0400

    updated lighthouse assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,12 @@ module.exports = {
       url: ['http://localhost:3000/'],
       startServerCommand: 'npm run start'
     },
+    assertions: {
+      'categories:performance': ['warn', { minScore: 0.9 }],
+      'categories:accessibility': ['error', { minScore: 0.9 }],
+      'categories:best-practices': ['error', { minScore: 0.9 }],
+      'categories:seo': ['error', { minScore: 0.9 }]
+    },
     upload: {
       target: 'temporary-public-storage'
     }
