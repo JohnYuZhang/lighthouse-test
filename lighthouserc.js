aaf1433a016f5812a806bcd5afb2009c7a4ad4c5
John Zhang <john.zhang.gyz@gmail.com>
2020-08-16 20:19:41 -0400

    added app token

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,11 +1,11 @@
 module.exports = {
-    ci: {
-      collect: {
-        url: ['http://localhost:3000/'],
-        startServerCommand: 'npm run start',
-      },
-      upload: {
-        target: 'temporary-public-storage',
-      },
+  ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'npm run start'
     },
-  };
+    upload: {
+      target: 'temporary-public-storage'
+    }
+  }
+}
