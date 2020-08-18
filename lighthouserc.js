dc61977e3b4d014295d2b93e8d79b8318e57d324
John Zhang <john.zhang.gyz@gmail.com>
2020-08-18 09:45:28 -0400

    updated assertions to match klick standards

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,10 +5,12 @@ module.exports = {
       startServerCommand: 'npm run start'
     },
     assertions: {
-      'categories:performance': ['warn', { minScore: 0.9 }],
+      'categories:performance': ['error', { minScore: 0.9 }],
       'categories:accessibility': ['error', { minScore: 0.9 }],
       'categories:best-practices': ['error', { minScore: 0.9 }],
-      'categories:seo': ['error', { minScore: 0.9 }]
+      'categories:seo': ['error', { minScore: 0.9 }],
+      'first-meaningful-paint': ['error',  { maxNumericValue: 3000}],
+      'interactive': ['error',  { maxNumericValue: 5000}]
     },
     upload: {
       target: 'filesystem',
