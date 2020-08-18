c25a20068dddad9f4c8e834d114af9d63de8b528
John Zhang <john.zhang.gyz@gmail.com>
2020-08-18 10:12:36 -0400

    updated assertion scores

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,9 +5,9 @@ module.exports = {
       startServerCommand: 'npm run start'
     },
     assertions: {
-      'categories:performance': ['error', { minScore: 0.9 }],
-      'categories:accessibility': ['error', { minScore: 0.9 }],
-      'categories:best-practices': ['error', { minScore: 0.9 }],
+      'categories:performance': ['error', { minScore: 1 }],
+      'categories:accessibility': ['error', { minScore: 1 }],
+      'categories:best-practices': ['error', { minScore: 1 }],
       'categories:seo': ['error', { minScore: 0.9 }],
       'first-contentful-paint': ['error',  { maxNumericValue: 100}],
       'interactive': ['error',  { maxNumericValue: 5000}]
