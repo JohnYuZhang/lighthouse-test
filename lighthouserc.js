671e11d1fc9adf650687dbf076c2ac634207dd10
John Zhang <john.zhang.gyz@gmail.com>
2020-08-18 10:27:58 -0400

    updated data struct

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,13 +4,15 @@ module.exports = {
       url: ['http://localhost:3000/'],
       startServerCommand: 'npm run start'
     },
-    assertions: {
-      'categories:performance': ['error', { minScore: 1 }],
-      'categories:accessibility': ['error', { minScore: 100 }],
-      'categories:best-practices': ['error', { minScore: 1 }],
-      'categories:seo': ['error', { minScore: 0.9 }],
-      'first-contentful-paint': ['error',  { maxNumericValue: 10}],
-      'interactive': ['error',  { maxNumericValue: 5000}]
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 1 }],
+        'categories:accessibility': ['error', { minScore: 100 }],
+        'categories:best-practices': ['error', { minScore: 1 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+        'first-contentful-paint': ['error',  { maxNumericValue: 10}],
+        'interactive': ['error',  { maxNumericValue: 5000}]
+      }
     },
     upload: {
       target: 'temporary-public-storage'
