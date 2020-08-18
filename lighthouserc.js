6772da4349f542d270707a1200b252cca8af4566
John Zhang <john.zhang.gyz@gmail.com>
2020-08-18 10:18:55 -0400

    updated score

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,14 +6,14 @@ module.exports = {
     },
     assertions: {
       'categories:performance': ['error', { minScore: 1 }],
-      'categories:accessibility': ['error', { minScore: 1 }],
+      'categories:accessibility': ['error', { minScore: 100 }],
       'categories:best-practices': ['error', { minScore: 1 }],
       'categories:seo': ['error', { minScore: 0.9 }],
-      'first-contentful-paint': ['error',  { maxNumericValue: 100}],
+      'first-contentful-paint': ['error',  { maxNumericValue: 10}],
       'interactive': ['error',  { maxNumericValue: 5000}]
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'temporary-public-storage'
     }
   }
 }
